c4a9b008af36f01ed98ea21100c90fb296a97bac
jcniabemr <john.connell@niab.com>
2023-01-23 19:28:57 +0000

    update

---
+++ checkM.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#SBATCH -p long
+#SBATCH -J checkM
+#SBATCH --mem=50G
+#SBATCH --cpus-per-task=24
+
+#Infiles should consist of a directory of genomes
+#Outdir should be a location for results to be written
+
+infile_dir=$1
+outdir=$2
+
+#Script to run checkM on genomes 
+
+checkm lineage_wf \
+-x .fna \
+$infile_dir \
+$outdir
+
+
