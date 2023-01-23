aefda7b9d7691602d4080047e5018347055dd824
jcniabemr <john.connell@niab.com>
2023-01-23 20:30:34 +0000

    update

---
+++ fasQC_analysis.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#SBATCH -J fastqc
+#SBATCH --partition=medium
+#SBATCH --mem=4G
+#SBATCH --cpus-per-task=4
+
+#Script to provide a fastQC analysis of raw and trimmed sequencing reads. 
+
+InfileF=$1
+InfileR=$2
+OutDir=$3
+
+WORK_DIR=/mnt/shared/scratch/jconnell/${SLURM_JOB_USER}_${SLURM_JOBID}_pseudomonasQC
+mkdir -p $WORK_DIR
+
+cp -r $InfileF $WORK_DIR
+cp -r $InfileR $WORK_DIR
+cd $WORK_DIR	
+
+fastqc *.gz
+unzip *.zip
+
+cp -r $WORK_DIR/*fastqc $OutDir
+rm -r $WORK_DIR
+
+
+
--- ps_assemble_annotate.sh
+++ ps_assemble_annotate.sh
@@ -17,44 +17,61 @@
 
 ####Seperate reads into folders with readname as foldername 
 
-# datadir=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/pseudomonas_data
+# datadir=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data
 # for data in 241278_241370 241185_241277 241464_241556 241371_241463; do 
-# i=$(ls -l /home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/pseudomonas_data/${data}/reads | awk '{print $9}' | grep -v "download_data\|run.txt" | cut -c 1-6)
+# i=$(ls -l /home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/${data}/ | awk '{print $9}' | grep -v "untrimmed_urls.txt" | cut -c 1-6)
 # 	for x in ${i}; do 
-# 		cd ${datadir}/${data}/reads 
+# 		cd ${datadir}/${data} 
 # 		mkdir -p ${x}
 # 		mv ${x}_* ${x}
 # 	done 
 # done 
 
-####Run fastQC on untrimmed reads
- 
+###Run fastQC on untrimmed reads
+
+
+
+for strain in 241185_241277; do
+	i=$(ls -l /home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/${strain} | awk '{print $9}' |  grep "241201")
+	for a in $i; do 
+		StrainPath=$(ls /home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/${strain}/${a}/*.gz)
+		OutDir=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/pseudomonas_data/${strain}/${a}/pre_trim_fastQC
+		mkdir -p $OutDir
+		ProgDir=/home/jconnell/git_repos/niab_repos/pseudomonas_analysis
+		sbatch $ProgDir/fasQC_analysis.sh $ReadsF $ReadsR $OutDir
+	done  
+done 
+
+
 # for strain in 241278_241370 241185_241277 241464_241556 241371_241463; do
-# 	a=$(ls -l /home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/${strain}/untrimmed_reads | awk '{print $9}')
-# 	for i in $a; do
-# 		StrainPathF=$(ls /home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/${strain}/untrimmed_reads/${i}/ | grep "_1_")
-# 		StrainPathR=$(ls /home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/${strain}/untrimmed_reads/${i}/ | grep "_2_")
-# 		infileF=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/${strain}/untrimmed_reads/${i}/$StrainPathF	
-# 		infileR=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/${strain}/untrimmed_reads/${i}/$StrainPathR	
-# 		OutDir=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/pseudomonas_data/${strain}/${i}/pre_trim_fastQC
+# 	i=$(ls -l /home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/${strain} | awk '{print $9}' |  grep -v "untrimmed_urls.txt")
+# 	for a in $i; do 
+# 		StrainPath=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/
+# 		ReadsF=$(ls $StrainPath/${strain}/${a}/* | grep "_1.fastq.gz")
+#     	ReadsR=$(ls $StrainPath/${strain}/${a}/* | grep "_2.fastq.gz")
+# 		OutDir=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/pseudomonas_data/${strain}/${a}/pre_trim_fastQC
 # 		mkdir -p $OutDir
-# 		ProgDir=git_repos/niab_repos/pipeline_canker_cherry/cherry_canker_pipeline
-# 		sbatch $ProgDir/fastqc_ud.sh $infileF $infileR $OutDir	
+# 		ProgDir=/home/jconnell/git_repos/niab_repos/pseudomonas_analysis
+# 		sbatch $ProgDir/fasQC_analysis.sh $ReadsF $ReadsR $OutDir
 # 	done  
 # done 
 
-#Trim adapter sequences from raw reads 
-IlluminaAdapters=/mnt/shared/scratch/agomez/apps/git_repos/bioinformatics_tools/SEQdata_qc/illumina_full_adapters.fa
-for strain in 241278_241370 241185_241277 241464_241556 241371_241463; do
-	file=$(ls /home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/${strain}/untrimmed_reads)
-	for x in $file; do
- 	StrainPath=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data
-  	ReadsF=$(ls $StrainPath/${strain}/*/${x} | grep "_1.fastq.gz")
-  	ReadsR=$(ls $StrainPath/${strain}/*/${x} | grep "_2.fastq.gz")
-  	echo $ReadsR
-  	echo $ReadsF
-  done 
-done 
+
+
+# #Trim adapter sequences from raw reads 
+# IlluminaAdapters=/mnt/shared/scratch/agomez/apps/git_repos/bioinformatics_tools/SEQdata_qc/illumina_full_adapters.fa
+# for strain in 241278_241370 241185_241277 241464_241556 241371_241463; do
+# 	file=$(ls /home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data/${strain}/untrimmed_reads)
+# 	for x in $file; do
+#  	StrainPath=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/raw_data
+#   	ReadsF=$(ls $StrainPath/${strain}/*/${x} | grep "_1.fastq.gz")
+#   	ReadsR=$(ls $StrainPath/${strain}/*/${x} | grep "_2.fastq.gz")
+#   	echo $ReadsR
+#   	echo $ReadsF
+#   done 
+
+
+# done 
 #    	F_outdir=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/pseudomonas_data/${strain}/${x}/trimmed_reads/F
 #   	R_outdir=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/pseudomonas_data/${strain}/${x}/trimmed_reads/R
 #   	mkdir -p $F_outdir $R_outdir
