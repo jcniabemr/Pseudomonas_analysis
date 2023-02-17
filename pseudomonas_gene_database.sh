05b8249550690283fd8a65fb865c7ae5af4eedf7
jcniabemr <john.connell@niab.com>
2023-02-17 00:53:46 +0000

    update

--- pseudomonas_gene_database.sh
+++ pseudomonas_gene_database.sh
@@ -14,18 +14,26 @@ datasets download genome accession "GCF_000007805.1" --include genome,gff3,cds
 unzip ncbi_dataset.zip
 rm ncbi_dataset.zip
 
-###Create NCBI database from DC3000 CDS
+####Create NCBI database from DC3000 CDS
 source miniconda3/bin/activate blast 
 cds=/home/jconnell/pseudomonas/gene_database/DC3000/ncbi_dataset/data/GCF_000007805.1
 makeblastdb -in $cds/cds_from_genomic.fna -parse_seqids -blastdb_version 5  -out $cds/DC3000_DB/DC3000 -dbtype nucl 
 
-###Filter Pss genomes for blast search 
+####Filter Pss genomes for blast search 
 for x in $(cat 49_56_63_70_77_passing_filter_IDres.txt | awk '($3 == "syringae") {print $1}' | cut -d "_" -f1); do 
 	genomes=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/pseudomonas_data/*/${x}/spades/filtered_contigs/renamed_files_contigs/ncbi_contigs
 	blastn -query ${genomes}/*.fa -db $cds/DC3000_DB/DC3000 -out pseudomonas/gene_database/blast_results/"$x"_hits_table.txt -evalue 1e-5 -outfmt 6
 done 
+####Filter Pss with additional species added (spacies names)
+for x in $(cat pseudomonas/gene_database/species_names); do
+    data=$(cat 49_56_63_70_77_passing_filter_IDres.txt | cut -f1-2 | grep -w $x | cut -f1 | cut -d "_" -f1)
+    for y in $data; do
+    	genomes=/home/jconnell/pseudomonas/pseudomonas_syringae_additional_sequencing/pseudomonas_data/*/${y}/spades/filtered_contigs/renamed_files_contigs/ncbi_contigs
+    	blastn -query ${genomes}/*.fa -db $cds/DC3000_DB/DC3000 -out pseudomonas/gene_database/blast_results/"$y"_hits_table.txt -evalue 1e-10 -outfmt 6
+	done
+done  
 
-###Create tables of gene matches per strain and concatenate 
+####Create tables of gene matches per strain and concatenate 
 for x in $(ls /home/jconnell/pseudomonas/gene_database/blast_results); do
 	cat /home/jconnell/pseudomonas/gene_database/blast_results/${x} \
 	| awk '{print $2}' | grep -Eo '[0-9]+$' | sort -nu | awk '{print "g"$0}' \
@@ -39,5 +47,6 @@ paste $(for x in $(ls /home/jconnell/pseudomonas/gene_database/temp_files | cut 
 for x in $(ls /home/jconnell/pseudomonas/gene_database/temp_files | cut -c 1-6); do 
     echo ${x} >> /home/jconnell/pseudomonas/gene_database/Ps_names
 done 
-cat Ps_names | paste -s Ps_names.txt
+cat Ps_names | paste -s > Ps_names.txt
 cat Ps_genes_DC3000.txt >> Ps_names.txt	
+paste DC3000_genes.txt Ps_names.txt > unordered_DB.txt
